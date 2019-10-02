ff02ffefeac3b40528456de2cc2c14282a4b3f89
SwanHub <jdprince555@gmail.com>
2019-10-02 00:51:01 -0600

    ticker route

--- go.mod
+++ go.mod
@@ -3,27 +3,9 @@ module github.com/SwanHub/custom-test-deploy
 go 1.13
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
-	github.com/coreos/etcd v3.3.10+incompatible // indirect
-	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
-	github.com/coreos/go-semver v0.2.0 // indirect
-	github.com/cpuguy83/go-md2man v1.0.10 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-gonic/gin v1.4.0
-	github.com/gopherjs/gopherjs v0.0.0-20190915194858-d3ddacdb130f // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/kisielk/gotool v1.0.0 // indirect
-	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/mitchellh/mapstructure v1.1.2 // indirect
-	github.com/neelance/astrewrite v0.0.0-20160511093645-99348263ae86 // indirect
-	github.com/neelance/sourcemap v0.0.0-20151028013722-8c68805598ab // indirect
-	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
-	github.com/spf13/cast v1.3.0 // indirect
-	github.com/spf13/cobra v0.0.3 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.2.1 // indirect
-	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
-	golang.org/x/crypto v0.0.0-20191001170739-f9e2070545dc // indirect
+	github.com/lib/pq v1.2.0
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	golang.org/x/sys v0.0.0-20191001151750-bb3f8db39f24 // indirect
-	golang.org/x/tools v0.0.0-20191001184121-329c8d646ebe // indirect
 )
--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"database/sql"
+	"time"
+	"fmt"
+
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func main(){
 	port := os.Getenv("PORT")
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -20,8 +30,36 @@ func main(){
 	router.LoadHTMLGlob("templates/*.html")
 	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "home.html", nil)
+	router.GET("/tick", func(c *gin.Context) {
+		if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error creating database table: %q", err))
+            return
+        }
+
+        if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error incrementing tick: %q", err))
+            return
+        }
+
+        rows, err := db.Query("SELECT tick FROM ticks")
+        if err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error reading ticks: %q", err))
+            return
+        }
+
+        defer rows.Close()
+        for rows.Next() {
+            var tick time.Time
+            if err := rows.Scan(&tick); err != nil {
+                c.String(http.StatusInternalServerError,
+                    fmt.Sprintf("Error scanning ticks: %q", err))
+                return
+            }
+            c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+		}
 	})
 
 	router.Run(":" + port)
