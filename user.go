c5c230b94e77e8fd926e4a53bb307f936e15bab6
SwanHub <jdprince555@gmail.com>
2019-10-03 18:41:14 -0600

    final seed

--- user.go
+++ user.go
@@ -52,46 +52,18 @@ func AllLanguages(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 		
 	// Seeding
-
-	db.Create(&Project{Name: "DataTrust Extension"})
-
-	// db.Model(language).Find(language)
-
-	var javascript Language
-	db.Where("name = ?", "Javascript").Find(&javascript)
 	var ruby Language
 	db.Where("name = ?", "Ruby").Find(&ruby)
+	
 	var rails Language
 	db.Where("name = ?", "Ruby on Rails").Find(&rails)
-	var react Language
-	db.Where("name = ?", "React.js").Find(&react)
-	var golang Language
-	db.Where("name = ?", "Go").Find(&golang)
-
-	var datatrust Project
-	db.Where("name = ?", "DataTrust").Find(&datatrust)
-	var extension Project
-	db.Where("name = ?", "DataTrust Extension").Find(&extension)
-	var mymdb Project
-	db.Where("name = ?", "MyMDb").Find(&mymdb)
-	var primary Project
-	db.Where("name = ?", "Primary Source").Find(&primary)
-
-	db.Model(datatrust).Association("Languages").Append(react)
-	db.Model(datatrust).Association("Languages").Append(rails)
-
-	db.Model(extension).Association("Languages").Append(javascript)
-	db.Model(extension).Association("Languages").Append(rails)
 	
-	db.Model(mymdb).Association("Languages").Append(ruby)
-	db.Model(mymdb).Association("Languages").Append(rails)
-
+	var primary Project
+	db.Where("name = ?", "PrimarySource").Find(&primary)
+	
 	db.Model(primary).Association("Languages").Append(ruby)
 	db.Model(primary).Association("Languages").Append(rails)
-
-	// find all projects associated with a language. 
-	// db.Model(&language).Related(&projects,  "Projects")
-
+	
 	// & == 'all'
 	var languages []Language
 	db.Find(&languages)
@@ -105,12 +77,12 @@ func NewLanguage(w http.ResponseWriter, r *http.Request) {
 		panic("Could not connect to the database")
 	}
 	defer db.Close()
-
+	
 	vars := mux.Vars(r)
 	name := vars["name"]
-
+	
 	newLanguage := map[string]string{"name": name}
-
+	
 	db.Create(&Language{Name: name})
 	json.NewEncoder(w).Encode(newLanguage)
 }
@@ -122,37 +94,48 @@ func DeleteLanguage(w http.ResponseWriter, r *http.Request) {
 		panic("Could not connect to the database")
 	}
 	defer db.Close()
-
+	
 	vars := mux.Vars(r)
 	name := vars["name"]
-
+	
 	var language Language
 	db.Where("name = ?", name).Find(&language)
-
+	
 	db.Delete(&language)
 	json.NewEncoder(w).Encode(name)
 }
 
 // func UpdateUser(w http.ResponseWriter, r *http.Request) {
-// 	db, err = gorm.Open("postgres", "host=localhost port=5431 user=jacksonprince password=JaQuEz11! dbname=test3 sslmode=disable")
-// 	// db, err = gorm.Open("postgres", os.Getenv("DATABASE_URL"))
-// 	if err != nil {
-// 		panic("Could not connect to the database")
-// 	}
-// 	defer db.Close()
-
-// 	vars := mux.Vars(r)
-// 	name := vars["name"]
-
-// 	var user Language
-// 	db.Where("name = ?", name).Find(&user)
-
-// 	user.Email = email
-
-// 	db.Save(&user)
-// 	json.NewEncoder(w).Encode(&user)
-// 	// fmt.Fprintf(w, "Updated Language")
-// }
-
+	// 	db, err = gorm.Open("postgres", "host=localhost port=5431 user=jacksonprince password=JaQuEz11! dbname=test3 sslmode=disable")
+	// 	// db, err = gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	// 	if err != nil {
+		// 		panic("Could not connect to the database")
+		// 	}
+		// 	defer db.Close()
+		
+		// 	vars := mux.Vars(r)
+		// 	name := vars["name"]
+		
+		// 	var user Language
+		// 	db.Where("name = ?", name).Find(&user)
+		
+		// 	user.Email = email
+		
+		// 	db.Save(&user)
+		// 	json.NewEncoder(w).Encode(&user)
+		// 	// fmt.Fprintf(w, "Updated Language")
+		// }
+		
 // To create a new instance in the database:
-	// db.Create(&Project{Name: "DataTrust"})
+// db.Create(&Project{Name: "DataTrust"})
+
+// Seeding looked liked this: 
+	// var datatrust Project
+	// db.Where("name = ?", "DataTrust").Find(&datatrust)
+
+	// var react Language
+	// db.Where("name = ?", "React.js").Find(&react)
+
+	// db.Model(datatrust).Association("Languages").Append(react)
+		
+		
